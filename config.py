f0cd4f036b89acabce9ceb57e35c0f459ab738a0
tutecano1995 <tutecano1995@gmail.com>
2020-08-14 21:11:17 -0300

    Add processing thread + remove async_consumer

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ def flag(name, default=False):
 DEBUG = flag("DEBUG", False)
 SYSTEMD = flag("SYSTEMD", False)
 
-URL_RPL_BACKEND = os.environ.get("URL_RPL_BACKEND", "http://www.rpl.codes")
-QUEUE_URL = os.environ.get("QUEUE_URL", "amqp://guest:guest@localhost:5672?heartbeat=5")
+URL_RPL_BACKEND = os.environ.get("URL_RPL_BACKEND", "http://localhost:8080")
+QUEUE_URL = os.environ.get("QUEUE_URL", "amqp://guest:guest@localhost:5672")
 QUEUE_ACTIVITIES_NAME = os.environ.get("QUEUE_ACTIVITIES_NAME", "hello")
 DOCKER_RUNNER_IMAGE = os.environ.get("DOCKER_RUNNER_IMAGE", "gcr.io/fiuba-rpl/rpl-2.0-runner:latest")
