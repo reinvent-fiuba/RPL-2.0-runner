ed9570c367e8696338b46dacf49afa9116b342cc
alejandro <levinasale@gmail.com>
2020-08-23 12:40:37 -0300

    Added custom_compileall.py file

---
+++ rpl_runner/runner-libs/python/custom_compileall.py
@@ -0,0 +1,31 @@
+import compileall
+
+compileall.compile_dir(".", force=True)
+
+import os
+import shutil
+
+DIR = os.getcwd()
+
+# Remove all python files
+for file in os.listdir(DIR):
+    if file.endswith(".py"):
+        os.remove(os.path.join(DIR, file))
+
+pycache_dir = os.path.join(DIR, "__pycache__")
+
+# rename pyc files extracting the cpython part
+# move files from __pycache__ to current dir
+for filename in os.listdir(pycache_dir):
+    if filename.endswith(".pyc"):
+        to_remove = filename.split(".")[-2]
+        new_name = filename.replace("." + to_remove, "")
+        os.rename(
+            os.path.join(pycache_dir, filename), os.path.join(pycache_dir, new_name)
+        )
+        shutil.move(os.path.join(pycache_dir, new_name), DIR)
+
+# delete old pycache files
+for folder in os.listdir(DIR):
+    if folder.startswith("__pycache__"):
+        os.rmdir(os.path.join(DIR, folder))
